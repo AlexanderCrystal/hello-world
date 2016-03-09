a61d244abe203335b37055e04d59218ff0387768
AlexanderCrystal <akurliandsky@gmail.com>
2016-03-09 11:24:00 +0200

    Create Example.java

---
+++ Example.java
@@ -0,0 +1,10 @@
+/*
+Это пример простой программы на Java.
+Присвойте файлу с исходным кодом имя Example.java.
+*/
+class Example {
+// Выполнение любой программы на Java начинается
+// с вызова метода maiп()
+puЫic static void maiп(Striпg args[]) {
+System.out.priпtln{"Java правит Интернетом!");
+}
